3afb4a0c8c0047189910f2e6d6c9e6db5aa46490
Anthony Fahden <afahden@gmail.com>
2021-11-02 13:30:21 -0700

    phase 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Cart from './components/Cart';
 import ProduceList from './components/ProduceList';
+import { useDispatch } from 'react-redux';
+import { populateProduce } from './store/produce';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(populateProduce());
+  }, [dispatch]);
+
   const [showCart, setShowCart] = useState(false);
   
   return (
--- src/store/produce.js
+++ src/store/produce.js
@@ -1,8 +1,28 @@
+import produceData from '../mockData/produce.json'
+
+const POPULATE = 'produce/POPULATE'
+
+
+//action creator
+export const populateProduce = () => {
+    console.log('!@#$action creator')
+  return {
+    type: POPULATE,
+    produce: produceData
+  };
+};
+
+//reducer
 export default function produceReducer(state = {}, action) {
     switch (action.type) {
+      case POPULATE:
+          const newState = {};
+          action.produce.forEach(produce => {
+              newState[produce.id] = produce;
+          })
+          return newState;
       default:
         return state;
     }
   }
 
-  
